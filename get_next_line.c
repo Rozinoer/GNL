1da8186bf81ecc9c63a162a9925ca9aa164c8b4c
Evgenii <56442478+Rozinoer@users.noreply.github.com>
2021-01-08 17:54:34 +0300

    Add files via upload
    right

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,28 @@
 /*   By: dmyesha <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 18:52:33 by dmyesha           #+#    #+#             */
-/*   Updated: 2021/01/05 18:57:25 by dmyesha          ###   ########.fr       */
+/*   Updated: 2021/01/08 17:20:21 by dmyesha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+
+int		free_str(char **str, int error)
+{
+	if (error == -1)
+	{
+		free(*str);
+		*str = NULL;
+		return (-1);
+	}
+	else
+	{
+		free(*str);
+		*str = NULL;
+	}
+	return (0);
+}
 
 char	*check_remainder(char *reaminder, char **line)
 {
@@ -36,36 +52,25 @@ char	*check_remainder(char *reaminder, char **line)
 	return (p_n);
 }
 
-void	str_join(char **line, char **buf)
-{
-	char *tmp;
-	char *tmp1;
-
-	tmp1 = *buf;
-	tmp = *line;
-	*line = ft_strjoin(*line, *buf);
-	free(tmp);
-	free(tmp1);
-}
-
-int		back(char **buf)
+void	str_join(char **str1, char **str2, int flag)
 {
 	char *tmp;
 
-	tmp = *buf;
-	free(tmp);
-	return (-1);
-}
-
-void    ream(char **p_n, char **reaminder)
-{
-	char *tmp;
-
-	**p_n = '\0';
-	++*p_n;
-	tmp = *reaminder;
-	*reaminder = ft_strjoin(*p_n, "");
-	free(tmp);
+	if (flag == 1)
+	{
+		tmp = *str1;
+		*str1 = ft_strjoin(*str1, *str2);
+		free_str(&tmp, 0);
+		free_str(str2, 0);
+	}
+	else if (flag == 2)
+	{
+		**str1 = '\0';
+		++*str1;
+		tmp = *str2;
+		*str2 = ft_strjoin(*str1, "");
+		free_str(&tmp, 0);
+	}
 }
 
 int		get_next_line(int fd, char **line)
@@ -82,20 +87,12 @@ int		get_next_line(int fd, char **line)
 	while (!p_n && byte_was_read > 0)
 	{
 		if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1)))
-		|| (byte_was_read = read(fd, buf, BUFFER_SIZE)) == -1)
-			return (back(&buf));
+				|| (byte_was_read = read(fd, buf, BUFFER_SIZE)) == -1)
+			return (free_str(&buf, -1));
 		buf[byte_was_read] = '\0';
 		if ((p_n = ft_strchr(buf, '\n')))
-			ream(&p_n,&remainder);
-		str_join(line, &buf);
+			str_join(&p_n, &remainder, 2);
+		str_join(line, &buf, 1);
 	}
-	if (buf && *buf)
-		printf("buffer = %s\n",buf);
-	else
-		printf("buffer is clean\n");
-	if (remainder && *remainder)
-		printf("reaminder = %s\n",remainder);
-	else
-		printf("reaminder is clean\n");
-	return ((byte_was_read == 0) ? 0 : 1);
+	return ((byte_was_read == 0) ? free_str(&remainder, 0) : 1);
 }
